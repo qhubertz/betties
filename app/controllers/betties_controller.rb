f6560b37cbe042645c41fc052c31a6b467bba2d3
Quinn Hubertz <qhubertz@gmail.com>
2014-09-18 23:41:27 -0700

    loads betties index action by reverse chronological order

--- app/controllers/betties_controller.rb
+++ app/controllers/betties_controller.rb
@@ -4,7 +4,7 @@ class BettiesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @betties = Betty.all
+    @betties = Betty.all.order("created_at DESC")
   end
 
   def show
